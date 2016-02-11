9110da972959f6e2eb077a060c4df163b30028c5
Shalaka Sinha <Shalaka@Shalaka-MacBook-Air.local>
2016-02-11 11:38:07 +0530

    repeat mode added

--- play.py
+++ play.py
@@ -1,7 +1,7 @@
 import subprocess
 from subprocess import check_output 
-import curses
 import psutil
+import time
 
 
 #check for mp3 files
@@ -17,10 +17,12 @@ print ("Enter 's' to start or enter song no. \n ")
 c = input("=>")
 while True:
 	if c == 's':
+		#start music player
 		count = 0
 		print ("playing ... "+l[0])
 		return_code = subprocess.Popen(["afplay", l[count]]).pid
 	elif c == 'n':
+		#play next song
 		if count+1 < n :
 			count = count + 1
 			print ("playing ... "+l[count])
@@ -30,6 +32,7 @@ while True:
 			print ("playing ... "+l[count])
 			return_code = subprocess.Popen(["afplay", l[count]]).pid
 	elif c == 'p':
+		#play previous song
 		if count-1 >= 0:
 			count = count-1
 			print ("playing ... "+l[count])
@@ -39,6 +42,7 @@ while True:
 			print ("playing ... "+l[count])
 			return_code = subprocess.Popen(["afplay", l[count]]).pid
 	elif c.isdigit():
+		#play the song having song no. 'c'
 		p = count
 		count = int(c)-1
 		if count < 0 or count >= n:
@@ -47,9 +51,14 @@ while True:
 		else : 
 			print ("playing ... "+l[count])
 			return_code = subprocess.Popen(["afplay", l[count]]).pid
-	c = input("Enter 'n' to choose next song , 'p' to choose previous song or song no. \n => ")
+	elif c == 'r':
+		#repeat the current song
+		print ("playing ..."+l[count])
+		return_code = subprocess.Popen(["afplay" , l[count]]).pid
+	c = input("Enter 'n' to choose next song , 'p' to choose previous song  , 'r' to repeat or song no. \n => ")
+	if c == None:
+		c = 'n'
 	p = psutil.Process(return_code)
 	#terminate the current process(stop the current song) and start the chosen one
 	p.terminate() 
-import time
 time.sleep(50)
